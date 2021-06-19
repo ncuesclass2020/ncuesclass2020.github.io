7c1203cfc68fd51d4894adc3ebf07647fa3b15c1
ncuesclass2020 <sunnytai@g.ncu.edu.tw>
2021-06-19 16:17:33 +0800

    Mobile adjust

--- YearBook/javascript/editor.js
+++ YearBook/javascript/editor.js
@@ -1,1 +1,1 @@
-﻿var pageEditor = [];
+﻿var pageEditor ={"setting":{"annoPlaying":"true","shoppingCartHTML":"false","shoppingCartOptinon":{"type":"PayPal","paypal":"","method":"POST","sandbox":"false","address":"","theme":"","body":"Hi xxx<br/>   I'm going to buy below product(s):<br/>    ${shopping}<br/>Full Name","showPrice":"true","showTime":"true"}}, "pageAnnos":[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]};
